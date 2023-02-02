b4985db0efb805652e283e114e261f90f56bf4c3
Krishna Murthy <crishna0401@gmail.com>
2023-02-02 15:48:26 +0530

    Update spectralvat.py

--- spectralvat.py
+++ spectralvat.py
@@ -1,13 +1,16 @@
 import numpy as np
 from visualclustering.VAT import VAT
 from visualclustering.iVAT import iVAT
-import networkx as nx
+from sklearn.metrics.pairwise import euclidean_distances
 
 class specvat():
-    def __init__(self, data, k):
+    def __init__(self, data, k, cp, ns):
         self.data = data
         self.k = k
+        self.cp = cp
+        self.ns = ns
         self.rs = self.dissimilarity()
+ 
 
     def vat(self):
         res = VAT(self.rs)
@@ -17,11 +20,70 @@ class specvat():
         res = iVAT(self.rs)
         return res
 
+    def MM(self,x, cp):
+        n, p = x.shape
+        m = np.ones(cp)
+        # d = np.sqrt(np.sum((x-x[0])**2, axis=1))
+        d = np.linalg.norm(x-x[0], axis=1, ord=2) # ord=2 is for euclidean distance
+        Rp = np.zeros((n, cp))
+        Rp[:,0] = d
+        for t in range(1, cp):
+            d = np.minimum(d, Rp[:,t-1])
+            m[t] = np.argmax(d)
+            # Rp[:,t] = np.sqrt(np.sum(((x[int(m[t])] - x)**2), axis=1))
+            Rp[:,t] = np.linalg.norm(x[int(m[t])] - x, axis=1)
+        return m, Rp
+
+    def MMRS(self,x, cp, ns):
+        n, p = x.shape
+        m, rp = self.MM(x, cp)
+        i = np.argmin(rp, axis=1)
+        smp = []
+        for t in range(cp):
+            s = np.where(i==t)[0]
+            nt = (np.ceil(ns*len(s)/n)).astype('int')
+            # randomly sample nt points from s
+            ind = np.random.choice(s, size=nt, replace=False)
+            smp.append(s[ind])
+        smp = [item for sublist in smp for item in sublist]
+        smp = list(set(smp))
+        return smp, rp, m
+
+
+    def myKNN(self,S, K, sigma=1.0):
+        N = len(S)
+        A = np.zeros((N,N))
+
+        for i in range(N):
+            dist_with_index = zip(S[i], range(N))
+            dist_with_index = sorted(dist_with_index, key=lambda x:x[0])
+            neighbours_id = [dist_with_index[m][1] for m in range(K+1)] # xi's k nearest neighbours
+
+            for j in neighbours_id: # xj is xi's neighbour
+                A[i][j] = np.exp(-S[i][j]/2/sigma/sigma)
+                A[j][i] = A[i][j] # mutually
+        return A
+
+
     def dissimilarity(self):
-        diag_mat = np.identity(self.data.number_of_nodes(),dtype=float)
-        lap_mat = diag_mat - nx.laplacian_matrix(self.data).toarray()
-        eig_values, eig_vecs = np.linalg.eigh(lap_mat)
-        sort_indices = np.argsort(eig_values)[::-1][:self.k]
+        if len(self.data) > self.ns:
+            print("data size is greater than 500, so using smart sampling")
+            # Sample data and obtain cluster information
+            smp, rp, m = self.MMRS(self.data, self.cp, self.ns)
+            # Compute pairwise distances between the sampled data
+            Similarity = euclidean_distances(self.data[smp], self.data[smp], squared=True)
+        else:
+            Similarity = euclidean_distances(self.data)
+
+        Adjacent = self.myKNN(Similarity, K=10)
+        degreeMatrix = np.sum(Adjacent, axis=1)
+        laplacianMatrix = np.diag(degreeMatrix) - Adjacent
+        sqrtDegreeMatrix = np.diag(1.0 / (degreeMatrix ** (0.5)))
+
+        Laplacian = np.dot(np.dot(sqrtDegreeMatrix, laplacianMatrix), sqrtDegreeMatrix)
+
+        eig_values, eig_vecs = np.linalg.eig(Laplacian)
+        sort_indices = np.argsort(eig_values)[:self.k]
         eigvec_stack = eig_vecs[:, sort_indices]
         dist_matrix = np.linalg.norm(eigvec_stack[:, None] - eigvec_stack, axis=2)
         dist_matrix = dist_matrix / np.max(dist_matrix)
